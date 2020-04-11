828e9bf8c3fca8f99b8c725fd7360d738bbe5fbe
Matias Rebori <martinezrebori@gmail.com>
2020-04-11 17:08:20 -0400

    clase Message para enviar mensajes entre clientes, utiliza JSON

---
+++ server/src/Message.java
@@ -0,0 +1,106 @@
+import org.json.simple.*;
+import org.json.simple.parser.*;
+public class Message {
+	Integer status;
+	String statusDetail;
+	Integer operation;
+	String message;
+	
+	public Message() {
+		
+	}
+	
+	/**
+	 * @param status estado del mensaje: 0 transaccion exitosa; -1 transanccion indeterminada; >=1 tira un codigo de error 
+	 * @param statusDetail ok si no existe error, detalle del error si existe
+	 * @param operation 1 ver clientes conectados, 2 iniciar llamada, 3 conversar, 4 terminar llamada
+	 * @param message mensaje enviado entre clientes
+	 */
+	public Message(Integer status, String statusDetail, Integer operation, String message) {
+		this.status = status;
+		this.statusDetail = statusDetail;
+		this.operation = operation;
+		this.message = message;
+	}
+
+	/**
+	 * Convierte un objeto Message a un String en formato JSON
+	 * @param Mensaje 
+	 * @return String JSON
+	 */
+	@SuppressWarnings("unchecked")
+	public String toJSON( ) {
+		JSONObject obj = new JSONObject();
+		obj.put("status", getStatus() );
+		obj.put("statusDetail", getStatusDetail() );
+		obj.put("operation", getOperation() );
+		obj.put("message", getMessage() );
+
+		return obj.toJSONString();
+	}
+
+	/**
+	 * Convierto un String en notacion JSON a un objeto Message
+	 * @param String 
+	 * @return Objeto Message
+	 * @throws ParseException
+	 */
+	public static Message toMessage(String str) throws ParseException {
+
+		JSONParser parser = new JSONParser();
+		Object obj = parser.parse(str.trim());
+
+		JSONObject jsonObject = (JSONObject) obj;
+		Long aux = (Long)jsonObject.get("status");
+		Integer status = new Integer( aux.intValue() );
+		String statusDetail = (String)jsonObject.get("statusDetail");
+		aux = (Long)jsonObject.get("operation");
+		Integer operation = new Integer( aux.intValue() );
+		String message = (String)jsonObject.get("message");
+
+		Message msg = new Message(status,statusDetail,operation,message);
+		return msg;
+
+	}
+	
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getStatusDetail() {
+		return statusDetail;
+	}
+
+	public void setStatusDetail(String statusDetail) {
+		this.statusDetail = statusDetail;
+	}
+
+	public Integer getOperation() {
+		return operation;
+	}
+
+	public void setOperation(Integer operation) {
+		this.operation = operation;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public static void main(String[] args) throws ParseException {
+		Message m = new Message( (Integer)1, "ok", (Integer)3 ,"Hola skere como estas");
+		String s = m.toJSON();
+		System.out.println(s);
+		Message me = toMessage(s);
+		System.out.println(  me.getStatus() + " " + me.getStatusDetail()   + " " + me.getOperation()  + " " + me.getMessage() );
+	}
+
+}
